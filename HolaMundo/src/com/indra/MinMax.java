54572c0f010f755e0fe1c66befde1cb7b3ecc1cb
Carlos Saltos <csaltos@gmail.com>
2021-11-24 13:33:36 +0200

    complete

---
+++ HolaMundo/src/com/indra/MinMax.java
@@ -0,0 +1,22 @@
+package com.indra;
+
+public class MinMax {
+
+	private Integer min;
+	private Integer max;
+	
+	public Integer getMin() {
+		return min;
+	}
+	public void setMin(Integer min) {
+		this.min = min;
+	}
+	public Integer getMax() {
+		return max;
+	}
+	public void setMax(Integer max) {
+		this.max = max;
+	}
+	
+	
+}
--- HolaMundo/src/com/indra/MoreStreams.java
+++ HolaMundo/src/com/indra/MoreStreams.java
@@ -1,5 +1,8 @@
 package com.indra;
 
+import java.util.IntSummaryStatistics;
+import java.util.Optional;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class MoreStreams {
@@ -14,6 +17,48 @@ public class MoreStreams {
 		
 		long conteo2 = Stream.of(1, 20, -5, 50).count();
 		System.out.println("Conteo2: " + conteo2);
+		
+		IntStream s = Stream.of("1", "5", "-9").mapToInt(Integer::valueOf);
+		IntSummaryStatistics resultados = s.summaryStatistics();
+		System.out.println("La suma es: " + resultados.getSum());
+		System.out.println("El promedio es: " + resultados.getAverage());
+		
+		Optional<MinMax> r = Stream.of(0, 5, 6, 7, -2).reduce(
+				Optional.<MinMax>empty(),
+				(acc, i) -> {
+					if (acc.isEmpty()) {
+						MinMax minMax = new MinMax();
+						minMax.setMin(i);
+						minMax.setMax(i);
+						return Optional.ofNullable(minMax);
+					} else {
+						MinMax minMax = acc.get();
+						int min = minMax.getMin();
+						int max = minMax.getMax();
+						MinMax nuevoMinMax = new MinMax();
+						nuevoMinMax.setMax(i > max ? i : max);
+						nuevoMinMax.setMin(i < min ? i : min);
+						return Optional.ofNullable(nuevoMinMax);
+					}
+				},
+				(i, j) -> {
+					if (i.isEmpty()) {
+						return j;
+					}
+					if (j.isEmpty()) {
+						return i;
+					}
+					int minI = i.get().getMin();
+					int maxI = i.get().getMax();
+					int minJ = j.get().getMin();
+					int maxJ = j.get().getMax();
+					MinMax nuevoMinMax = new MinMax();
+					nuevoMinMax.setMin(minI < minJ ? minI : minJ);
+					nuevoMinMax.setMax(maxI > maxJ ? maxI : maxJ);
+					return Optional.ofNullable(nuevoMinMax);
+				});
+		r.ifPresent(minMax -> System.out.println("min: " + minMax.getMin()));
+		r.ifPresent(minMax -> System.out.println("max: " + minMax.getMax()));
 	}
 
 }
